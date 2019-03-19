d49441a7644ba2accd540140920193b093e57d4f
Cadwyl Lorelei <clorelei@e3r7p3.21-school.ru>
2019-03-19 11:51:27 +0300

    init

---
+++ tests.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+BSQ=./bsq
+TEST_DIR=tests
+
+passed_tests=1
+for file in $TEST_DIR/*.map; do
+	DIFF=$($BSQ $file 2>&1 | diff ${file%.map}.output -)
+	if [ -z "$DIFF" ]
+	then
+		echo "$file OK"
+	else
+		echo "$file FAIL"
+		echo "$DIFF"
+		exit 1
+	fi
+	passed_tests=$((passed_tests+1))
+done
+exit 0
