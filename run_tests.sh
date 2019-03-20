0e7b8284706788499aace1fc1a31287dedf1e8e7
Alexey Dubrov <vhsw@ya.ru>
2019-03-20 17:11:05 +0300

    test multiple files handling

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-BSQ=../BSQ/bsq
-TEST_DIR=tests
+BSQ=./bsq
 
-MAP_GENERATOR=./map_generator.pl
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+TEST_DIR=$DIR/tests
+MAP_GENERATOR=$DIR/map_generator.pl
 passed_tests=1
 FILE_ERROR="map error" # "wrong file path"
 MAP_ERROR="map error"
@@ -20,6 +21,16 @@ for file in $TEST_DIR/*.map; do
 	fi
 	passed_tests=$((passed_tests+1))
 done
+
+DIFF=$($BSQ $TEST_DIR/*.map 2>&1 | diff $TEST_DIR/multiple.output - )
+if [ -z "$DIFF" ]
+then
+	echo "OK multiple files"
+else
+	echo "FAIL multiple files"
+	echo "$DIFF"
+	exit 1
+fi
 
 OUT=$($BSQ jaosjdfoajrotiasdgaergwasdf 2>&1 > /dev/null )
 if [ "$OUT" == "$FILE_ERROR" ]
@@ -41,31 +52,38 @@ else
 	exit 1
 fi
 
-for i in {1..100}
+echo "Tesing random maps. It might take some time..."
+for i in {1..99}
 do
-	$MAP_GENERATOR $i $i $i | $BSQ
-	$MAP_GENERATOR $i $i $((i/10)) | $BSQ
-	$MAP_GENERATOR $i $i 0 | $BSQ
-	echo "OK map $i $i $((i/10))"
+	printf "\rMap size $i\r"
+	if	[ $($MAP_GENERATOR $i $i $i | $BSQ | wc -c) != $((($i + 1) * $i)) ] || \
+		[ $($MAP_GENERATOR $i $i $((i/10)) | $BSQ | wc -c) != $((($i + 1) * $i)) ] || \
+		[ $($MAP_GENERATOR $i $i 0 | $BSQ | wc -c) != $((($i + 1) * $i)) ]
+	then
+		echo "FAIL Map $i x $i"
+		exit 1
+	fi
+	
 done
-
+echo "OK random maps"
+echo "Tesing big maps. It might take some time..."
 # uncomment if you wanna make you computer cry
 for i in 100 1000 # 10000
 do
-	$MAP_GENERATOR 1 $i 0 | $BSQ
-	$MAP_GENERATOR 1 $i $((i/10)) | $BSQ
-	$MAP_GENERATOR 1 $i $i | $BSQ
-	echo "OK map 1 $i"
-
-	$MAP_GENERATOR $i 1 0 | $BSQ
-	$MAP_GENERATOR $i 1 $((i/10)) | $BSQ
-	$MAP_GENERATOR $i 1 $i | $BSQ
-	echo "OK map $i 1"
-
-	$MAP_GENERATOR $i $i 0 | $BSQ
-	$MAP_GENERATOR $i $i $((i/10)) | $BSQ
-	$MAP_GENERATOR $i $i $i | $BSQ
-	echo "OK map $i $i"
+	printf "\rMap size $i\r"
+	if	[ $($MAP_GENERATOR 1 $i $i | $BSQ | wc -c) != $(((1 + 1) * $i)) ] || \
+		[ $($MAP_GENERATOR 1 $i $((i/10)) | $BSQ | wc -c) != $(((1 + 1) * $i)) ] || \
+		[ $($MAP_GENERATOR 1 $i 0 | $BSQ | wc -c) != $(((1 + 1) * $i)) ] || \
+		[ $($MAP_GENERATOR $i 1 1 | $BSQ | wc -c) != $((($i + 1) * 1)) ] || \
+		[ $($MAP_GENERATOR $i 1 0 | $BSQ | wc -c) != $((($i + 1) * 1)) ] || \
+		[ $($MAP_GENERATOR $i $i $i | $BSQ | wc -c) != $((($i + 1) * $i)) ] || \
+		[ $($MAP_GENERATOR $i $i $((i/10)) | $BSQ | wc -c) != $((($i + 1) * $i)) ] || \
+		[ $($MAP_GENERATOR $i $i 0 | $BSQ | wc -c) != $((($i + 1) * $i)) ]
+	then
+		echo "FAIL Map $i x $i"
+		exit 1
+	fi
 done
+echo "OK big maps   "
 echo "ALL TESTS PASS"
 exit 0
